2d57ac998fe578f77e32493207e8d118b8ba429f
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-10 11:08:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send('one');
 });
 app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+res.send('one');
+    
 });
 app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+res.send('one');
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
