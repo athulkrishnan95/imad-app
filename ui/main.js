953934288097b27e00e1867ea376aaa8f779af01
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-11 12:27:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,9 @@ window.onload = function(){
     console.log('Loaded!');
     var heading=document.getElementById('heading');
 heading.onmouseover=function(){mouseOver()};
+heading.onmouseout=function(){
+heading.style.color="black";};
+};
 function mouseOver(){
     heading.style.color="yellow";
 }
-};
