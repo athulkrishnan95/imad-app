5e4e6c50963e307df806c1239132af0926159e20
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-11 12:39:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,21 @@ window.onload = function(){
     console.log('Loaded!');
     var heading=document.getElementById('heading');
 heading.onmouseover=function(){mouseOver()};
+
 heading.onmouseout=function(){
-heading.style.color="black";};
+    heading.style.color="black";
+    
 };
+
+heading.onclick=function(){
+ blink();
+};
+function blink(){
+    heading.style.fontSize = "large";
+    setTimeout(function() {}, 100);
+    heading.style.fontSize="x";
+}
 function mouseOver(){
     heading.style.color="yellow";
 }
+};
