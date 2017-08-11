b605d7d437b7abf6e7df61f0071eccd437acfcd0
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-11 12:23:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
-console.log('Loaded!');
+
 window.onload = function(){
+    console.log('Loaded!');
     var heading=document.getElementById('heading');
 heading.onclick=function(){mouseOver()};
 function mouseOver(){
     alert('hi');
 }
-}
+};
