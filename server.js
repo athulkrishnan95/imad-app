5f2b1a8cfd7435c8336b39132ad9d6c40457d99a
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-15 19:06:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ He is the closest a superhero could be to a common man.</li></ol></p>`
     }
     
 };
-
+var counter=0;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -53,6 +53,10 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
  res.send(createTemplate(articles[articleName]));    
+});
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
 });
 
 function createTemplate(data){
