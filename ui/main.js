f6806ea8042bcb01b0e7da85346aed5208980523
athulkrishnan95 <30854579+athulkrishnan95@users.noreply.github.com>
2017-08-15 19:19:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,16 +18,27 @@ heading.onclick=function(){
 
 
 
-var counter=0;
-
 var button=document.getElementById('counter');
 button.onclick=function(){
-    counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+ 
+    request.onreadystatechange=function(){
+      if (request.readyState === XMLHttpRequest.DONE){
     
+        if (request.status===200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+        
+    }
+    };
+        
 }
   ;
+  request.open('GET','http://athulpallipurth.imad.hasura-app.io/counter',true);
+  request.send(null);
+  
    /* marginLeft=marginLeft+10;
     heading.style.marginLeft=marginLeft+'px';*/
 
